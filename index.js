512b2d2d9006941da23c87b002b9715698be806c
Abdyl-Aziz <azizshihabov@mail.ru>
2022-01-17 21:41:49 +0300

    feat: add request-promise npm module

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const weatherRequest = require('./requests/weather.request');
+
+const openWeatherAPI = '125067d076d7a01052d6ab356be5c2af';
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', (req, res) => {
-  res.end('Hello from NodeJS');
-  
+  res.render('index');
+});
+
+app.post('/', (req, res) => {
+  const { city } = req.body;
+
+  weatherRequest(city);  
+  res.render('index');
 });
 
 app.listen(3000, () => {
---
+++ requests/weather.request.js
@@ -0,0 +1,10 @@
+const rp = require('request-promise');
+
+module.exports = async function(city) {
+  if(!city) {
+    throw new Error('Имя города не может быть пустым')
+  }
+
+  console.log(city);
+  
+}
